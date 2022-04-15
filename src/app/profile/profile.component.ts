a8e0c46be1d87035c1677d400a378e5a51af7b30
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-15 11:51:28 +0300

    add profile

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  // providers:[ProfileService]
+})
+export class ProfileComponent implements OnInit {
+
+  user: any;
+  userRepos:any;
+  username: string = 'linetlucy-genchabe'
+  imageWidth: number = 220;
+  imageHeight: number = 250;
+  
+
+  constructor(private profileService: ProfileService, private http:HttpClient) { 
+    this.user=profileService.getUser()
+    
+  }
+
+  findUser () {
+    this.profileService.UpdateUser(this.username);
+
+    this.profileService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+
+    this.profileService.getUserRepos().subscribe(repos => {
+      console.log(repos);
+      this.userRepos = repos;
+    })
+  }
+
+  ngOnInit() {
+    this.findUser()
+  }
+
+}

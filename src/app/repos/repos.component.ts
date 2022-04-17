e2d615959f985cc3459464350afdbac83dd8957a
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-17 23:03:06 +0300

    edit findrepo method

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-repos',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+  repoitems:any=["items"];
+  repoName:string='Quotes-app';
+  
+  
 
-  constructor() { }
+  constructor(private profileService:ProfileService, private http:HttpClient) {
+    
 
+   }
+
+  
+   findRepo () {
+     this.profileService.UpdateRepo(this.repoName);
+     this.profileService.searchrepos().subscribe((repo: { [x: string]: any; })=> {
+       console.log(this.repoitems);
+       this.repoitems= repo['items']; 
+       console.log(this.repoitems);
+      // console.log(repo["items"]);
+      // this.repoitems= repo["items"]; //this is the new
+       // this.item = repo;
+       
+     })
+   }
   ngOnInit(): void {
+    this.findRepo
   }
 
 }

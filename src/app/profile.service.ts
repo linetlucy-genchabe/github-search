bb289fc9bd700117c2879507b897242604d4e106
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-17 20:22:27 +0300

    edit functions

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { observable } from 'rxjs';
 
 
 
@@ -9,24 +11,28 @@ import { Injectable } from '@angular/core';
 export class ProfileService {
   username!: string;
   repoName!: string;
-  clientId: string = "c789323b20a2d31dc9fa";
-  clientSecret: string = "83e428e5ed1c99fded8ab02686753f74656f9f6b";
-  apikey:string='be05ad82ef1dec303061648a172836e81613bc78';
-  constructor(private http: HttpClient) { 
-    console.log('service is now ready');
-  }
+  
 
+  private _Url='https://api.github.com/users';
+
+  constructor(private http: HttpClient) { 
+    // console.log('service is now ready');
+    {this.username='linetlucy-genchabe';}
+
+  }
   getUser() {
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientId + "&client_secret=" + this.clientSecret);
+    return this.http.get("https://api.github.com/users/" + this.username );
   }
 
   getUserRepos() {
-    return this.http.get('https://api.github.com/users/' + this.username + '/repos'+ "?client_id=" + this.clientId + "&client_secret=" + this.clientSecret);
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos');
   }
+
+  
 
   searchrepos() {
     return this.http.get('https://api.github.com/search/repositories?q=' + this.repoName, ({
-      headers: new HttpHeaders({Authorization: `token ${this.apikey}`})
+      headers: new HttpHeaders({Authorization: `token ${environment.apikey}`})
     }))
   }
 
@@ -38,3 +44,12 @@ export class ProfileService {
     this.repoName = repo;
   }
 }
+
+
+// ngOnInit() {
+//   // GET request with response type <any>
+//   this.http.get<any>('https://api.github.com/users').subscribe((data) => {
+//     this.username = data.total;
+//     console.log(this.username);
+//   });
+// }
